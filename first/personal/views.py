bb5608df55d94febcd1d63ce6179c2587ec55d15
hasanpasha <bashahsn22@gmail.com>
2020-04-09 14:51:04 +0300

    add test paragraph

--- first/personal/views.py
+++ first/personal/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<a><h1>HOME</h1></a><h2>HOME PAGE</h2><p>HELLO, World!</p>")
+    return HttpResponse("<p>Test</p><a><h1>HOME</h1></a><h2>HOME PAGE</h2><p>HELLO, World!</p>")
